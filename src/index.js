3b020c444a060a92e601d9a9d44f8df70356d903
Adriana Monster <AdrianaMonster@profretail.local>
2021-09-08 14:20:21 -0400

    added the base url to the browserrouter in index,js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/js/myscripts"
 
 ReactDOM.render(
-< BrowserRouter >
+< BrowserRouter basename={process.env.PUBLIC_URL} >
     <App />
     </BrowserRouter>,
   document.getElementById('root')
