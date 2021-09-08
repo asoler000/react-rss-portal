486ca516734616bc13f1f939948ffc6087a84182
Adriana Monster <AdrianaMonster@profretail.local>
2021-09-08 11:16:04 -0400

    added the exact url base path to the index.css page not app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import {BrowserRouter} from "react-dom"
 import './App.css';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
@@ -9,22 +8,22 @@ import Dashboard from './Dashboard';
 
 
 const App = () => (
-< BrowserRouter basename={process.env.PUBLIC_URL}>
+<Router>
     <div className="App">
     <div class="container-fluid">
  
    
       
           <Switch>
-            <Route exact path='/SignIn' component={SignIn} />
+            <Route exact path='/' component={SignIn} />
             <Route path="/SignIn" component={SignIn} />
             <Route path="/Dashboard" component={Dashboard} />
-            <div>Hello World</div>
+     
        
     
           </Switch>
 
-    </div></div></BrowserRouter>
+    </div></div></Router>
   );
 
   
